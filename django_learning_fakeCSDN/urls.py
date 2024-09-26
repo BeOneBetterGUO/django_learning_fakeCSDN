4d425a2bbcc1d2579d2cfdd171cbabe8d8fd9e23
Grant <2693470816@qq.com>
2024-09-26 22:52:48 +0800

    modify urls.py

--- django_learning_fakeCSDN/urls.py
+++ django_learning_fakeCSDN/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.http import HttpResponse
+from django.urls import path, include
+from book import views
 
+# https://www.baidu.com/s?wd=python
+
+def index(request):
+    return HttpResponse('Hello, Django!')
 urlpatterns = [
+    path('',index),
     path('admin/', admin.site.urls),
+    path('book/',include("book.urls")),
+    path('movie/', include("movie.urls"))
+
 ]
